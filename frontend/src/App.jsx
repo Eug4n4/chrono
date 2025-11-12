8772c8983de5f90600d36915fd2991c5826255b4
Eug4n4 <Eug4n4@users.noreply.github.com>
2025-11-12 14:06:40 +0200

    layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,32 @@ import PasswordReset from "./pages/auth/PasswordReset";
 import EmailVerification from "./pages/auth/EmailVerification";
 import CalendarPage from "./pages/calendar/CalendarPage";
 
+function Layout() {
+    return (
+        <div className="app">
+            <Outlet />
+        </div>
+    );
+}
+
 function App() {
     return (
         <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="password-reset/:token" element={<SetNewPassword />} />
-            <Route path="password-reset" element={<PasswordReset />} />
-            <Route
-                path="/verify-email/:token"
-                element={<EmailVerification />}
-            />
-            <Route path="/calendar" element={<CalendarPage />} />
-            <Route path="*" element={<NotFound />} />
+            <Route element={<Layout />}>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                    path="password-reset/:token"
+                    element={<SetNewPassword />}
+                />
+                <Route path="password-reset" element={<PasswordReset />} />
+                <Route
+                    path="/verify-email/:token"
+                    element={<EmailVerification />}
+                />
+                <Route path="/calendar" element={<CalendarPage />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
         </Routes>
     );
 }
