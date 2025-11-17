4723319a4a8e4e497e80cf2cf23efd4bd0a524fb
Eug4n4 <Eug4n4@users.noreply.github.com>
2025-11-17 16:31:25 +0200

    Separate child schema for each event type

--- backend/src/models/Event.js
+++ backend/src/models/Event.js
@@ -1,13 +1,32 @@
 import mongoose from "mongoose";
 
-const EventSchema = new mongoose.Schema({
-    name: String,
-    start: Date,
-    end: Date,
-    type: { type: String, enum: ["meeting", "task", "reminder"] },
-    tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag" }],
-});
+const options = { discriminatorKey: "type" };
 
+const EventSchema = new mongoose.Schema(
+    {
+        name: String,
+        start: Date,
+        tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag" }],
+    },
+    options,
+);
 const Event = mongoose.model("Event", EventSchema);
 
+const ArrangementEvent = Event.discriminator(
+    "arrangement",
+    new mongoose.Schema({ end: Date }),
+    options,
+);
+const ReminderEvent = Event.discriminator(
+    "reminder",
+    new mongoose.Schema({ remindAfter: Date }),
+    options,
+);
+const TaskEvent = Event.discriminator(
+    "task",
+    new mongoose.Schema({ end: Date, description: String }),
+    options,
+);
+
+export { ArrangementEvent, ReminderEvent, TaskEvent };
 export default Event;
